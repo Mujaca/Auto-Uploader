f3fbc07b9933079a5c59adb75a9c9c0393ffb54e
Mujaca <admin@mujaca.de>
2020-12-11 18:43:57 +0100

    added start arguments

--- entry.js
+++ entry.js
@@ -1,3 +1,5 @@
+//Start Arguments
+var args = process.argv.slice(2);
 async function boot(){
     //Loads the Timestamps and the Log File Writing
     require('./console/logger')
@@ -12,7 +14,15 @@ async function boot(){
     //Loads the Filewatcher
     const {watcher} = require("./uploader/index")
     global.watcher = watcher;
-    new watcher('./');
+    new watcher(args[0]);
+    //Loads up the Auto Updater
+    const autoupdater = require("./functions/autoupdate");
+    autoupdater.updater();
+    //Everything loaded!
+    setTimeout(() => {
+        console.clear();
+        console.log("Mujaca`s AutoUploader")
+    }, 1000);
 }
 
 boot();
