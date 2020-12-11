ee4a98b0c5be0ed025fabe5b34c75e6f705b39c3
Mujaca <admin@mujaca.de>
2020-12-11 18:43:39 +0100

    change to only check the version

--- functions/autoupdate.js
+++ functions/autoupdate.js
@@ -9,44 +9,18 @@ setInterval(async () => {
     if (download == false) {
     var version = config.getConfig('version');
     download = true;
-    axios.get('http://mujaca.de/versions/uploader.json')
+    axios.get('http://mujaca.de/versions/uploaderv2.json')
     .catch((error) => {
         // console.error(error)
     })
     .then(async (response) => {
         if(version !== response.data.version){
-            var url = response.data.download;
-            const file = await require('axios')({
-                url,
-                method: 'GET',
-                responseType: 'stream'
-            })
-            const writer = fs.createWriteStream('./update.zip');
-            file.data.pipe(writer)
-            fs.createReadStream('./update.zip')
-            .pipe(unzipper.Extract({ path: './' })).on('finish', async () => {
-                config.writeConfig('version', response.data.version);
-                fs.unlink('./update.zip', (err) => {
-                    if(err) throw err;
-                })
-                console.log("Update installed. Restarting ...")
-                var cmd = "node " + './';
-                args.forEach((arg) => {
-                    cmd = cmd + " " + arg;
-                })
-                var exec = require('child_process').exec;
-                await exec('npm install', function() {
-
-                })
-                exec(cmd, function () {
-                  process.kill();
-                });
-            });
-            
+            console.log("A new Version is available!")
+            console.log(`Download it from here: ${response.data.download}`)
         }
     })
 }
-}, 60000);
+}, 1000);
 }
 
 exports.updater = main;
